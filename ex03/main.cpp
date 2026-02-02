c51463edfeba51d680663d83cd77924013f2c956
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-02-02 23:21:00 +0100

    ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 21:09:08 by plichota          #+#    #+#             */
-/*   Updated: 2026/02/02 23:19:33 by plichota         ###   ########.fr       */
+/*   Updated: 2026/02/02 23:20:15 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,40 @@ int main()
         std::cout << YELLOW << "---- Intern creates robotomy: success ----" << RESET << std::endl;
         AForm* form = someRandomIntern.makeForm("robotomy request", "Bender");
         if (form)
+        {
             bureaucrat1.signForm(*form);
+            bureaucrat1.executeForm(*form);
+        }
         delete form;
     }
     {
         std::cout << YELLOW << "---- Intern creates shrubbery: success ----" << RESET << std::endl;
         AForm* form = someRandomIntern.makeForm("shrubbery creation", "Home");
         if (form)
+        {
             bureaucrat1.signForm(*form);
+            bureaucrat1.executeForm(*form);
+        }
         delete form;
     }
     {
         std::cout << YELLOW << "---- Intern creates presidential: success ----" << RESET << std::endl;
         AForm* form = someRandomIntern.makeForm("presidential pardon", "Pres");
         if (form)
+        {
             bureaucrat1.signForm(*form);
+            bureaucrat1.executeForm(*form);
+        }
         delete form;
     }
     {
         std::cout << YELLOW << "---- Intern creates unknown: fail ----" << RESET << std::endl;
         AForm* form = someRandomIntern.makeForm("unknown", "Random");
         if (form)
+        {
             bureaucrat1.signForm(*form);
+            bureaucrat1.executeForm(*form);
+        }
         delete form;
     }
 }
